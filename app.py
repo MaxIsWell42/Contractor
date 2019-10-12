cbfa1b4851ab67ffe0c3b407f5814f94d547f408
HexSeal <maxfin13@gmail.com>
2019-10-11 18:37:45 -0700

    [update] app.py for products, need to update the rest of the files until everything has the correct variables and implimentation before I add extras

--- app.py
+++ app.py
@@ -6,90 +6,88 @@ import os
 
 # app.py
 
-host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/producter')
 client = MongoClient(host=host)
 db = client.get_default_database()
-playlists = db.playlists
+products = db.products
 comments = db.comments
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists.find()) 
+def products_index():
+    """homepage"""
+    return render_template('products_index.html', products=products.find()) 
 
-@app.route('/playlists/new')
-def playlists_new():
-    """Create a new playlist."""
-    return render_template('playlists_new.html', playlist={}, title='New Playlist')
+@app.route('/products/new')
+def products_new():
+    """Create a new product."""
+    return render_template('products_new.html', product={}, title='New product')
 
-@app.route('/playlists', methods=['POST'])
-def playlists_submit():
-    """Submit a new playlist."""
-    playlist = {
+@app.route('/products', methods=['POST'])
+def products_submit():
+    """Submit a new product."""
+    product = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
-        'videos': request.form.get('videos').split(),
         'created_at': datetime.now()
     }
-    print(playlist)
-    playlist_id = playlists.insert_one(playlist).inserted_id
-    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+    print(product)
+    product_id = products.insert_one(product).inserted_id
+    return redirect(url_for('products_show', product_id=product_id))
 
-@app.route('/playlists/<playlist_id>')
-def playlists_show(playlist_id):
-    """Show a single playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
-    return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
+@app.route('/products/<product_id>')
+def products_show(product_id):
+    """Show a single product."""
+    product = products.find_one({'_id': ObjectId(product_id)})
+    product_comments = comments.find({'product_id': ObjectId(product_id)})
+    return render_template('products_show.html', product=product, comments=product_comments)
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+@app.route('/products/<product_id>/edit')
+def products_edit(product_id):
+    """Show the edit form for a product."""
+    product = products.find_one({'_id': ObjectId(product_id)})
+    return render_template('products_edit.html', product=product, title='Edit product')
 
-@app.route('/playlists/<playlist_id>', methods=['POST'])
-def playlists_update(playlist_id):
-    """Submit an edited playlist."""
-    updated_playlist = {
+@app.route('/products/<product_id>', methods=['POST'])
+def products_update(product_id):
+    """Submit an edited product."""
+    updated_product = {
         'title': request.form.get('title'),
         'description': request.form.get('description'),
         'videos': request.form.get('videos').split()
     }
-    playlists.update_one(
-        {'_id': ObjectId(playlist_id)},
-        {'$set': updated_playlist})
-    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+    products.update_one(
+        {'_id': ObjectId(product_id)},
+        {'$set': updated_product})
+    return redirect(url_for('products_show', product_id=product_id))
 
 # app.py
 ...
-@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
-def playlists_delete(playlist_id):
-    """Delete one playlist."""
-    playlists.delete_one({'_id': ObjectId(playlist_id)})
-    return redirect(url_for('playlists_index'))
+@app.route('/products/<product_id>/delete', methods=['POST'])
+def products_delete(product_id):
+    """Delete one product."""
+    products.delete_one({'_id': ObjectId(product_id)})
+    return redirect(url_for('products_index'))
 
-@app.route('/playlists/comments', methods=['POST'])
+@app.route('/products/comments', methods=['POST'])
 def comments_new():
     """Submit a new comment."""
     comment = {
         'title': request.form.get('title'),
         'content': request.form.get('content'),
-        'playlist_id': ObjectId(request.form.get('playlist_id'))
+        'product_id': ObjectId(request.form.get('product_id'))
     }
     print(comment)
-    comment_id = comments.insert_one(comment).inserted_id
-    return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
+    return redirect(url_for('products_show', product_id=request.form.get('product_id')))
 
-@app.route('/playlists/comments/<comment_id>', methods=['POST'])
+@app.route('/products/comments/<comment_id>', methods=['POST'])
 def comments_delete(comment_id):
     """Action to delete a comment."""
     comment = comments.find_one({'_id': ObjectId(comment_id)})
     comments.delete_one({'_id': ObjectId(comment_id)})
-    return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
+    return redirect(url_for('products_show', product_id=comment.get('product_id')))
 
 if __name__ == '__main__':
   app.run(debug=False, host='0.0.0.0', port=os.environ.get('PORT', 5000))
